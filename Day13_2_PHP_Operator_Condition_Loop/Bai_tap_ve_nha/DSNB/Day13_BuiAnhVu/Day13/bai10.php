9b345a593c35a9955d804de13129a2be2d946bd7
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2019-12-19 10:32:37 +0700

    REview bai tap ngay 13

--- Day13_2_PHP_Operator_Condition_Loop/Bai_tap_ve_nha/DSNB/Day13_BuiAnhVu/Day13/bai10.php
+++ Day13_2_PHP_Operator_Condition_Loop/Bai_tap_ve_nha/DSNB/Day13_BuiAnhVu/Day13/bai10.php
@@ -1,45 +1,46 @@
 <meta charset="utf-8">
 <title>Bài 10</title>
 <style>
-    #den{
+    #den {
         height: 70px;
         width: 70px;
         background: black;
     }
-    #trang{
+
+    #trang {
         height: 70px;
         width: 70px;
         background: white;
     }
-    div{
+
+    div {
         display: inline-block;
-        border-style: solid ;
+        border-style: solid;
         border-color: black;
         boder-width: 1px;
     }
 </style>
 <?php
-for($i = 8; $i >= 1; $i--){
-    if($i %2 != 0){
-        for($j = 8; $j >= 1; $j--){
-            if($j % 2 != 0){
-                echo"<div id='den'></div>";
-            }
-            else{
-                echo"<div id='trang'></div>";
-            }
+for ($i = 8; $i >= 1; $i--) {
+  if ($i % 2 != 0) {
+    for ($j = 8; $j >= 1; $j--) {
+      if ($j % 2 != 0) {
+        echo "<div id='den'></div>";
+      } else {
+        echo "<div id='trang'></div>";
+      }
     }
-    echo "<br>";}
-    else{
-        for($j = 8; $j >= 1; $j--){
-            if($j % 2 != 0){
-                echo"<div id='trang'></div>";
-            }
-            else{
-                echo"<div id='den'></div>";
-            }
-        }echo"<br>";
+    echo "<br>";
+  } else {
+    for ($j = 8; $j >= 1; $j--) {
+      if ($j % 2 != 0) {
+        echo "<div id='trang'></div>";
+      } else {
+        echo "<div id='den'></div>";
+      }
     }
+    echo "<br>";
+  }
 
 }
 ?>
--- Day13_2_PHP_Operator_Condition_Loop/Bai_tap_ve_nha/DSNB/PHP1019E2_NguyenHuuHung_Ngay13/PHP1019E2_NguyenHuuHung_Ngay13/bai4.php
+++ Day13_2_PHP_Operator_Condition_Loop/Bai_tap_ve_nha/DSNB/PHP1019E2_NguyenHuuHung_Ngay13/PHP1019E2_NguyenHuuHung_Ngay13/bai4.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 function xuat($n = 50)
 {
 
