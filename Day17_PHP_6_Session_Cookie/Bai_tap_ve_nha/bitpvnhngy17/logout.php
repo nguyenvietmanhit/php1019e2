43a7d506cc53db8c8a1979e50b3071ba4426d21a
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2020-01-10 17:29:37 +0700

    update

--- Day17_PHP_6_Session_Cookie/Bai_tap_ve_nha/bitpvnhngy17/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['success'] = "Đăng xuất thành công";
-header("location: bai_tap_ve_nha_day_17_1.php");
-exit();
-?>
--- Day17_PHP_6_Session_Cookie/Bai_tap_ve_nha/bitpvnhngy17/logout_3.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['username4']);
-setcookie('name','admin',time() + -1);
-header("location: bai_tap_ve_nha_day_17_3.php");
-exit();
-?>
-<!--<a href="bai_tap_ve_nha_day_17_3.php">login</a>-->
