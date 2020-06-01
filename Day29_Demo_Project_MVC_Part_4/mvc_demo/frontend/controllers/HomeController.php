1c9bb9ace83a17da7f759f0b66ce7044f3dd4ecf
Manh <nvmanh@cmc.com.vn>
2020-06-01 23:13:18 +0700

    update

--- Day29_Demo_Project_MVC_Part_4/mvc_demo/frontend/controllers/HomeController.php
+++ Day29_Demo_Project_MVC_Part_4/mvc_demo/frontend/controllers/HomeController.php
@@ -2,6 +2,7 @@
 require_once 'controllers/Controller.php';
 
 class HomeController extends Controller {
+  
   public function index() {
     $this->content = $this->render('views/homes/index.php');
     require_once 'views/layouts/main.php';
--- Day29_Demo_Project_MVC_Part_4/mvc_demo/frontend/views/homes/index.php
+++ Day29_Demo_Project_MVC_Part_4/mvc_demo/frontend/views/homes/index.php
@@ -1,7 +1,116 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: nvmanh
- * Date: 5/11/2020
- * Time: 3:02 PM
- */
+<div class="content-timeline">
+  <!--Timeline header area start -->
+  <div class="post-list-header">
+    <h1 class="post-list-title">
+      <a href="#" class="link-category-item">Sản phẩm nổi bật</a>
+    </h1>
+  </div>
+
+  <!--Timeline items start -->
+  <div class="timeline-items">
+
+    <!--ITEM-->
+    <div class="timeline-item">
+      <div class="timeline-left">
+        <div class="timeline-left-wrapper">
+          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
+                class="material-icons">&#xE894;</i></a>
+          <span class="timeline-date">3 phút trước</span>
+        </div>
+      </div>
+      <div class="timeline-right">
+        <div class="timeline-post-image">
+          <a href="#">
+            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
+                 width="260">
+          </a>
+        </div>
+        <div class="home-page timeline-post-content">
+          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
+          <a href="not-defined.html">
+            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
+          </a>
+          <div class="product-price timeline-post-info">
+            12.490.000₫
+          </div>
+          <div class="timeline-post-info">
+            <a href="#" class="product-cart">
+              Thêm vào giỏ hàng
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--END ITEM-->
+
+    <!--ITEM-->
+    <div class="timeline-item">
+      <div class="timeline-left">
+        <div class="timeline-left-wrapper">
+          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
+                class="material-icons">&#xE894;</i></a>
+          <span class="timeline-date">3 phút trước</span>
+        </div>
+      </div>
+      <div class="timeline-right">
+        <div class="timeline-post-image">
+          <a href="#">
+            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
+                 width="260">
+          </a>
+        </div>
+        <div class="home-page timeline-post-content">
+          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
+          <a href="not-defined.html">
+            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
+          </a>
+          <div class="product-price timeline-post-info">
+            12.490.000₫
+          </div>
+          <div class="timeline-post-info">
+            <a href="#" class="product-cart">
+              Thêm vào giỏ hàng
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--END ITEM-->
+
+    <!--ITEM-->
+    <div class="timeline-item">
+      <div class="timeline-left">
+        <div class="timeline-left-wrapper">
+          <a href="#" class="timeline-category" data-zebra-tooltip title="Thể thao"><i
+                class="material-icons">&#xE894;</i></a>
+          <span class="timeline-date">3 phút trước</span>
+        </div>
+      </div>
+      <div class="timeline-right">
+        <div class="timeline-post-image">
+          <a href="#">
+            <img src="http://tevratgundogdu.com/works/ideabox-html-template/img/news-test-images/news-img8.jpg"
+                 width="260">
+          </a>
+        </div>
+        <div class="home-page timeline-post-content">
+          <a href="#" class="timeline-category-name font-arial">Điện thoại</a>
+          <a href="not-defined.html">
+            <h3 class="timeline-post-title">Điện thoại Samsung Galaxy Note 10 Lite</h3>
+          </a>
+          <div class="product-price timeline-post-info">
+            12.490.000₫
+          </div>
+          <div class="timeline-post-info">
+            <a href="#" class="product-cart">
+              Thêm vào giỏ hàng
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--END ITEM-->
+
+  </div>
+  <!--Timeline items end -->
+</div>
